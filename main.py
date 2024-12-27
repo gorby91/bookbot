54bbb23d91a4960f8f18ef57df4b8bed4288f781
gorby91 <sam.voller@gmail.com>
2024-12-27 11:55:19 +1100

    refactor code after viewing solution

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ def main():
     file_contents = get_book_text(path)
     word_count = count_words(file_contents)
     character_count = count_characters(file_contents)
-    character_list = list_dict(character_count)
+    character_list = chars_to_sorted_list(character_count)
     print_report(path, word_count, character_list)
 
 def get_book_text(path):
     with open(path) as f:
-        file_contents = f.read()
-    return file_contents
-
+        return f.read()
+    
 def count_words(file_contents):
     words = file_contents.split()
     return len(words)
@@ -27,23 +26,19 @@ def count_characters(file_contents):
 def sort_on(dict):
     return dict["num"]
 
-def list_dict(dict):
-    char_list = []
-    for char in dict:
-        if char.isalpha():
-            char_list.append({"char": char, "num": dict[char]})
-    char_list.sort(reverse=True, key=sort_on)
-    return char_list
-
-
-def print_char_report(character_list):
-    for item in character_list:
-        print(f"The {item["char"]} character was found {item["num"]} times")
+def chars_to_sorted_list(dict):
+    sorted_list = []
+    for ch in dict:
+        if ch.isalpha():
+            sorted_list.append({"char": ch, "num": dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 def print_report(path, word_count, character_list):
     print(f"--- Begin report of {path} ---")
     print(f"{word_count} found in the document\n")
-    print_char_report(character_list)
+    for item in character_list:
+        print(f"The {item["char"]} character was found {item["num"]} times")
     print("--- End report ---")
 
 main()
